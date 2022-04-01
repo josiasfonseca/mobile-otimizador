764b1187adf882a284f23dcb30ef9d9493da0ce7
Josias Fonseca <josiasmegabit@hotmail.com>
2022-03-31 22:36:52 -0300

    COMMIT FINAL

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default function App() {
     <NavigationContainer >
         <Drawer.Navigator
           drawerContent={props => <DrawerComponent {...props} />}
-          initialRouteName="Importador"
+          initialRouteName="Login"
           screenOptions={screensOptions}
           backBehavior='history'
         >
